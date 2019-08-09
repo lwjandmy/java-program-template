5b044516185815020ebaeeb7be658fe108c2f15e
lwjandmy <lwjandmy@qq.com>
2019-08-09 20:48:08 +0800

    爬页面

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,12 +1,38 @@
 
 import org.junit.Test;
+import org.apache.commons.io.IOUtils;
+import java.io.File;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class AppTest {
 
+	public void log(Object obj) {
+		System.out.println(obj);
+	}
+
 	@Test
-	public void test() {
-	
-		System.out.println("test");
+	public void test() throws Exception {
+		String html = IOUtils.toString(this.getClass().getResourceAsStream("/web.html"), "UTF-8");
+			
+		Document doc = Jsoup.parse(html);
+		
+		Elements list = doc.select(".index_zb_main");
+		
+		for (Element main : list) {
+			Elements title = main.select(".index_intro");
+			
+			Elements href = main.select(".index_img a");
+			
+			Elements time = main.select(".index_time span");
+			
+			log(String.format("%s\t%s\t%s", time.text(), title.text(), href.attr("href")));
+		}
+		
+		
 	}
 
 }
